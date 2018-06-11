281855abeb9ffde0cf74812155a7a2ea774c91dc
Florian <pfistfl@users.noreply.github.com>
2018-06-11 11:43:39 +0200

    Update checkHeight_script.R
    Del comments

--- checkHeight_script.R
+++ checkHeight_script.R
@@ -3,6 +3,9 @@
 # Date: 24.10.2017
 # Author: Jann Goschenhofer
 
+# FIXME Comment Function
+# FIXME Comment Inputs
+# FIXME Comment Function Outputs
 library(dplyr)
 
 #Initialize the data frame
@@ -10,16 +13,17 @@ age = c(19, 22, 21, 23, 22, 20, 28, 25)
 weight = c(50, 75, 80, 56, 75, 58, 65, 82)
 height = c(1.66, 1.78, 1.90, 1.72, 1.83, 1.68, 1.70, 1.85)
 sex = c("F", "M", "M", "F", "M", "F", "F", "M")
-
 students = data.frame(cbind(age, weight, height, sex))
 students = transform(students, age = as.numeric(as.character(age)))
 students = transform(students, height = as.numeric(as.character(height)))
 students = transform(students, weight = as.numeric(as.character(weight)))
-
 students$name = c("Maria", "Franz", "Peter", "Lisa", "Hans", "Eva", "Mia", "Karl")
-#Use a dataframe containing the information about the students as input
+
+# Input: 
+# Argument1, class of argument1 object
+## Missing Comment ##
 checkHeight3 <- function(students.input = students){
-  #Calculate the gender specific means
+  ## Missing Comment ##
   women_mean_height = as.numeric(students %>%
                              group_by(sex) %>%
                              summarise(mean(height)) %>%
@@ -30,19 +34,18 @@ checkHeight3 <- function(students.input = students){
                                    summarise(mean(height)) %>%
                                    filter(sex == "M") %>%
                                    select("mean(height)"))
-  #initialize an empty vector to save the genderspecific height differnces
+  ## Missing Comment ##
   height_vector = c() 
-  #apply a function to the rows of the input dataframe 
+   ## Missing Comment ##
   height_vector = apply(students, MARGIN = 1, 
     FUN = function(student){
     #substract the gender specific means from the individuals to get height differnces
     (if (student["sex"] == "M") men_mean_height - as.numeric(student["height"]) 
     else women_mean_height - as.numeric(student["height"]) ) 
     } ) 
-  #create the final dataframe containing name od the students and the height differnces 
-  #multiple height differences by 100 to get values in cm
+  ## Missing Comment ##
   result.frame = data.frame("name" = students$name, "sexspec_height_diff" = height_vector*100)
-  #return the dataframe
+  ## Missing Comment ##
   return(result.frame)
 }
 
