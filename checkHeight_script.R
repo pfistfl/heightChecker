447df3a5f4ba941bd6922bdc8f58ef6f53c0039a
Susanne Dandl <dandl.susanne@gmail.com>
2018-11-03 10:45:42 +0100

    Incoperate meaningful comments to function and helping function, second part of issue fix #1

--- checkHeight_script.R
+++ checkHeight_script.R
@@ -21,10 +21,18 @@ students$name = c("Maria", "Franz", "Peter", "Lisa", "Hans", "Eva", "Mia", "Karl
 
 
 checkHeight3 = function(students.input = students){
-
+  #' calculate height difference of persons in a data.frame to 
+  #' the average height of their gender
+  #' @param students.input ('data.frame') \cr
+  #'   data.frame of students, needs columns height, sex and name 
+  #' @return ('data.frame') \cr
+  #'   returns data.frame with names and difference to average gender height
+  
+  # create return dataframe 
   result.frame = data.frame(matrix(NA, nrow = nrow(students.input), ncol = 2))
   colnames(result.frame) = c("name", "difference")
 
+  # calculate average height by gender
   male.mean = students.input %>%
     filter(sex == "M") %>%
     summarise(mean = mean(height))
@@ -32,10 +40,12 @@ checkHeight3 = function(students.input = students){
     filter(sex == "F") %>%
     summarise(mean = mean(height))
 
+  # calculate diff for each person
   height.diff = apply(students.input, 1, function(person, avg_male, avg_female) {
     getDiff(avg_male, avg_female, data.frame(t(person)))
   }, "avg_male" = male.mean$mean, "avg_female" = female.mean$mean)
   
+  # fill return data.frame
   result.frame$name = students.input$name
   result.frame$difference = height.diff
   return(result.frame)
@@ -43,6 +53,15 @@ checkHeight3 = function(students.input = students){
 }
 
 getDiff = function(avg_male, avg_female, person) {
+  #' function to calculate difference of height of one person
+  #' to average height based on gender
+  #' @param avg_male ('float(1)')\cr
+  #'  average male height
+  #' @param avg_female ('float(1)')\cr
+  #'  average female height
+  #' @return ('float(1)')\cr
+  #'  difference of persons height to average 
+  
   mean_height = ifelse(person$sex == "F", avg_female, avg_male)
   diff = (as.numeric(as.character(person$height)) - mean_height)*100
   return(diff)
